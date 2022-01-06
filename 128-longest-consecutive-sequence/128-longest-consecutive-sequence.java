43860efa3ce16fb42e61b506d32b532f4fe22a51
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-01-06 13:56:26 +0530

    Time: 80 ms (38.35%), Space: 60.8 MB (15.22%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,34 +1,34 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
         
-        if(nums.length==0)
+        if(nums.length==0)           //edge case
             return 0;
         
         int max = Integer.MIN_VALUE;
         HashMap<Integer,Integer> map = new HashMap<>();
         
         for(int n:nums){
-            if(map.containsKey(n) == false){
+            if(map.containsKey(n) == false){        //if it is a duplicate then ignore
                 int sp = n;
                 int ep = n;
                 
-                if(map.containsKey(n-1)==true){
+                if(map.containsKey(n-1)==true){        //update start point
                     sp = sp - map.get(n-1);
                 }
                 
-                if(map.containsKey(n+1)==true){
+                if(map.containsKey(n+1)==true){       //update end point
                     ep = ep + map.get(n+1);
                 }
                 
-                int newlen = ep-sp+1;
+                int newlen = ep-sp+1;               //find the new length
                 
-                map.put(sp,newlen);
-                map.put(ep,newlen);
+                map.put(sp,newlen);               //update the length of start point in the map
+                map.put(ep,newlen);               //update the length of end point in the map
                 
-                if(sp!=n && ep!=n)
-                    map.put(n,1);
+                if(sp!=n && ep!=n)                // if it is not the start point and not the end point      
+                    map.put(n,1);                //length you can put any->doesnt matter
                 
-                max = Math.max(max,newlen);
+                max = Math.max(max,newlen);        //finding the max consequence
             }  
         }
     return max;
