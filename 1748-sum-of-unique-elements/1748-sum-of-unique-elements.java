c112f5e1e37d955f923326f96373b2f97eac7f95
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-01-06 22:46:39 +0530

    Time: 0 ms (100.00%), Space: 38.7 MB (11.94%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -1,23 +1,41 @@
+// class Solution {
+//     public int sumOfUnique(int[] nums) {                   //HashMap approach
+//         int sum = 0 ;
+//         HashMap<Integer,Integer> map = new HashMap<>();
+        
+//         for(int i=0;i<nums.length;i++){
+//             if(map.containsKey(nums[i])==false){
+//                 map.put(nums[i],1);
+//             }
+//             else{
+//                 int count = map.get(nums[i]);
+//                 count++;
+//                 map.put(nums[i],count);
+//             }
+//         }
+        
+//         for(int key : map.keySet()){
+//             if(map.get(key)==1)
+//                 sum=sum+key;
+//         }
+//         return sum;
+//     }
+// }
+
 class Solution {
-    public int sumOfUnique(int[] nums) {
-        int sum = 0 ;
-        HashMap<Integer,Integer> map = new HashMap<>();
+    public int sumOfUnique(int[] nums) {    
+        int sum = 0;
+        int map[] = new int[101];       //same approach as that of car-pooling question
         
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])==false){
-                map.put(nums[i],1);
+        for (int num : nums) {
+            if (map[num] == 0) {
+                sum += num; 
+            } else if (map[num] == 1) {
+                sum -= num;
             }
-            else{
-                int count = map.get(nums[i]);
-                count++;
-                map.put(nums[i],count);
-            }
+            map[num] += 1;
         }
         
-        for(int key : map.keySet()){
-            if(map.get(key)==1)
-                sum=sum+key;
-        }
         return sum;
     }
 }
