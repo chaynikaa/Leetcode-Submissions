490b13b718ea68a4427ca5e9084b96bb6e1b332a
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-01-06 14:25:53 +0530

    Time: 2 ms (92.93%), Space: 39.3 MB (39.88%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
@@ -1,21 +1,17 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        
-        int n = nums1.length;
-        int m = nums2.length;
         int len=0;
-        
-        if(n>m)
-            len=m;
+        if(nums1.length>nums2.length)
+            len=nums2.length;
         else
-            len=n;
+            len=nums1.length;
         
         int[] ans = new int[len];
-        
+    
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        int i=0,j=0,k=0;
         
+        int i=0,j=0,k=0;
         while(i<nums1.length && j<nums2.length){
             if(nums1[i]>nums2[j])
                 j++;
@@ -30,6 +26,7 @@ class Solution {
         int[] a = new int[k];
         for(int l=0;l<k;l++)
             a[l] = ans[l];
+        
         return a;
     }
 }
