5c77c5ccf5e847faf9bdd629db8630061f04971d
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-01-06 17:11:07 +0530

    Time: 18 ms (24.10%), Space: 53.4 MB (13.94%) - LeetHub

--- 554-brick-wall/554-brick-wall.java
+++ 554-brick-wall/554-brick-wall.java
@@ -3,14 +3,14 @@ class Solution {
         
         HashMap<Integer,Integer> map = new HashMap<>();
         int sum;
-        int max=0;
+        int max=0;             //for storing min no. of cuts made at index
         
-        for(int i=0;i<wall.size();i++){
+        for(int i=0;i<wall.size();i++){   
             sum=0;
-            for(int j=0;j<wall.get(i).size()-1;j++){
-                sum = sum + wall.get(i).get(j);
+            for(int j=0;j<wall.get(i).size()-1;j++){//keep in mind to run the code from size-1.cant make cut atlast     
+                sum = sum + wall.get(i).get(j);      //storing the sum for first row 
                 
-                if(map.containsKey(sum)==false)
+                if(map.containsKey(sum)==false)      
                     map.put(sum,1);
                 else{
                      int count = map.get(sum);
