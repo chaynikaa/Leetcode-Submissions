0aeaf485f8938e315158829da6155e6814f016d1
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-01-06 21:36:54 +0530

    Time: 2 ms (74.98%), Space: 41.1 MB (23.55%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -3,13 +3,13 @@ class Solution {
       
       int stops[] = new int[1001]; 
         for (int t[] : trips) {
-          stops[t[1]] += t[0];
-          stops[t[2]] -= t[0]; 
+          stops[t[1]] += t[0];   //adding the passengers at the time of pick up in a new array
+          stops[t[2]] -= t[0];   //subtracting the passenger at the time of drop in the array
        }
-      
-      for(int number : stops){
-          capacity-=number;
-          if(capacity<0)
+       
+      for(int number : stops){   //traversing the array
+          capacity = capacity-number;
+          if(capacity<0)               //limit exceeded
               return false;;
       }
    return true;
