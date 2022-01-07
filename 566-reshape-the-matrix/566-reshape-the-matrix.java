0f68fa3f93e1ae6047f72c51fa09c7ec9b5a21be
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-01-07 12:26:35 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (92.42%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.java
+++ 566-reshape-the-matrix/566-reshape-the-matrix.java
@@ -26,3 +26,14 @@ class Solution {
     }
 }
 
+// class Solution {                        //APPROACH-2
+//     public int[][] matrixReshape(int[][] nums, int r, int c) {
+//         int m = nums.length, n = nums[0].length, o = m * n;
+//         if (r * c != o) 
+//             return nums;
+//         int[][] res = new int[r][c];
+//         for (int i = 0; i < o; i++) 
+//             res[i / c][i % c] = nums[i / n][i % n];
+//         return res;        
+//     }
+// }
