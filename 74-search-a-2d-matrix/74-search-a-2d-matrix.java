aed56da930ecb8fe9264af8d8f5b8d1903670965
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-01-08 14:43:15 +0530

    Time: 0 ms (100.00%), Space: 38.4 MB (59.33%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -8,20 +8,43 @@ class Solution {
 //             }
 //         }
 //         return false;
-//     }                                                           //one approach is consider the whole as 1 array 
-                                                                   //apply binary search
+//     }                                                           
     
-        public boolean searchMatrix(int[][] matrix, int target) {   //start searching from top right corner
-            int i = 0;
-            int j = matrix[0].length-1;
-            while(i<matrix.length && j>=0){
-                if(matrix[i][j]==target)
+        public boolean searchMatrix(int[][] matrix, int target) {   //one approach is consider the whole as 1array 
+                                                                   //apply binary search
+            int m = matrix.length;;
+            int n = matrix[0].length;
+            int left = 0;
+            int right = m*n - 1;
+            
+            // if(m==0)
+            //     return false;
+                        
+            while(left<=right){
+                int mid = (left+right)/2;
+                
+                if(matrix[mid/n][mid%n]==target)
                     return true;
-                else if(matrix[i][j]>target)
-                    j--;
+                else if(matrix[mid/n][mid%n]>target)
+                    right=mid-1;
                 else
-                    i++;
+                    left=mid+1;
             }
+        
             return false;
     }
+    
+    //        public boolean searchMatrix(int[][] matrix, int target) {   //start searching from top right corner
+    //         int i = 0;
+    //         int j = matrix[0].length-1;
+    //         while(i<matrix.length && j>=0){
+    //             if(matrix[i][j]==target)
+    //                 return true;
+    //             else if(matrix[i][j]>target)
+    //                 j--;
+    //             else
+    //                 i++;
+    //         }
+    //         return false;
+    // }
 }
