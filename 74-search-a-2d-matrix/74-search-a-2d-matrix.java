1f25f146a3133c8439a3e6316c76fc9f433e74bd
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-01-08 14:43:26 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (29.21%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -16,10 +16,7 @@ class Solution {
             int n = matrix[0].length;
             int left = 0;
             int right = m*n - 1;
-            
-            // if(m==0)
-            //     return false;
-                        
+
             while(left<=right){
                 int mid = (left+right)/2;
                 
