ccb3155b7d79c3adf1bd8ca8e29ea41b532a0be3
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-01-09 12:10:12 +0530

    Time: 11 ms (70.56%), Space: 45.5 MB (23.45%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,24 +1,41 @@
 class Solution {
     public int firstUniqChar(String s) {
         
-        HashMap<Character,Integer> map = new HashMap<>();
+        int a[] = new int[27];                        //array approach
         
         for(int i=0;i<s.length();i++){
-            if(map.containsKey(s.charAt(i))==false)
-                map.put(s.charAt(i),1);
-            else{
-                int count = map.get(s.charAt(i));
-                count++;
-                map.put(s.charAt(i),count);
-            }
+            char ch = s.charAt(i);
+            a[ch-'a']+=1;
         }
         
         for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(map.get(ch)==1){
+            char c=s.charAt(i);
+            if(a[c-'a']==1)
                 return i;
-            }
         }
      return -1;   
     }
+    
+//         public int firstUniqChar(String s) {
+        
+//         HashMap<Character,Integer> map = new HashMap<>();   //hashmap approach
+        
+//         for(int i=0;i<s.length();i++){
+//             if(map.containsKey(s.charAt(i))==false)
+//                 map.put(s.charAt(i),1);
+//             else{
+//                 int count = map.get(s.charAt(i));
+//                 count++;
+//                 map.put(s.charAt(i),count);
+//             }
+//         }
+        
+//         for(int i=0;i<s.length();i++){
+//             char ch=s.charAt(i);
+//             if(map.get(ch)==1){
+//                 return i;
+//             }
+//         }
+//      return -1;   
+//     }
 }
