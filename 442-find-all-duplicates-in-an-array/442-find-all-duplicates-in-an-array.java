a5b488c8f44228ad9b33355b92bef71c81282740
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-01-12 01:03:27 +0530

    Time: 10 ms (41.01%), Space: 63.1 MB (25.41%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.java
@@ -1,23 +1,41 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        
-        int[] arr = new int[nums.length];   //using array for doing hashing   
-        List<Integer>ans = new ArrayList<>();  //similar as car-pooling ques
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==nums.length)
-                arr[0]++;
-            else
-                arr[nums[i]]++;
+        List<Integer> ans = new ArrayList<>();
+
+        for (int num : nums) {
+            nums[Math.abs(num) - 1] *= -1;
         }
-       
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]>1)
-                ans.add(i);
+
+        for (int num : nums) {
+            if (nums[Math.abs(num) - 1] > 0) {
+                ans.add(Math.abs(num));
+                nums[Math.abs(num) - 1] *= -1;
+            }
         }
-        
-        if(arr[0]>1)
-            ans.add(nums.length);
-        
+
         return ans;
     }
 }
+// class Solution {
+//     public List<Integer> findDuplicates(int[] nums) {
+        
+//         int[] arr = new int[nums.length];   //using array for doing hashing   
+//         List<Integer>ans = new ArrayList<>();  //similar as car-pooling ques
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i]==nums.length)
+//                 arr[0]++;
+//             else
+//                 arr[nums[i]]++;
+//         }
+       
+//         for(int i=1;i<arr.length;i++){
+//             if(arr[i]>1)
+//                 ans.add(i);
+//         }
+        
+//         if(arr[0]>1)
+//             ans.add(nums.length);
+        
+//         return ans;
+//     }
+// }
