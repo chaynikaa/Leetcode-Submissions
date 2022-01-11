2efda142b911450154f20536c0bf0fe14163dd41
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-01-11 17:28:36 +0530

    Time: 0 ms (100.00%), Space: 36.7 MB (67.42%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -2,17 +2,17 @@ class Solution {
     
 public List<Integer> getRow(int rowIndex) {
         List<Integer> curRow = new ArrayList<Integer>();
-        curRow.add(1);
+        curRow.add(1);  //first addition of 1
         if (rowIndex == 0) 
             return curRow;
         
-        List<Integer>prevRow = getRow(rowIndex - 1);
+        List<Integer>prevRow = getRow(rowIndex - 1);  //getting the last prev row before the nth one
     
         for (int i = 0; i<prevRow.size()-1; i++){
-            curRow.add(prevRow.get(i)+prevRow.get(i+1));
+            curRow.add(prevRow.get(i)+prevRow.get(i+1));  //addition tht values and adding it to the curr list
         }
     
-        curRow.add(1);
-        return curRow;
+        curRow.add(1);   //last addition of 1
+        return curRow; 
     }
 }
