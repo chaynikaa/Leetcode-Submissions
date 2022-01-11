7521b0342282cf5d45757f1acb65f652d8be5a61
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-01-11 17:56:00 +0530

    Time: 23 ms (58.14%), Space: 51.6 MB (57.35%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -5,17 +5,17 @@ class Solution {
         int ans = 0;
         for(int i=0;i<bank.length;i++){
             String s = bank[i];
-            int count = 0;
-            for(int j=0;j<s.length();j++){
+            int count = 0;     
+            for(int j=0;j<s.length();j++){     
                 char ch = s.charAt(j);
                 if(ch == '1')
-                    count++;
+                    count++;    //counting the no. of security devices
             }
             if(count!=0)
-                arr[m++]=count;
+                arr[m++]=count;   //putting in the array
         }
         for(int i=0;i<m-1;i++)   {
-            ans = ans + arr[i]*arr[i+1];
+            ans = ans + arr[i]*arr[i+1];   //calc the ans
         }     
         return ans;
         
