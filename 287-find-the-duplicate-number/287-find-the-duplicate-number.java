b538122fe39b76100a43507c3e99607317837a1e
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-01-11 17:01:47 +0530

    Time: 5 ms (80.11%), Space: 73.3 MB (28.43%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,24 +1,36 @@
-class Solution {
     
     //1- sort the array and then bring out the duplicate
     //2- using set. if the set already contains the element return that element
     
-    public int findDuplicate(int[] nums) {       //Fluyd's algorithm
-         int tortoise = nums[0];
-        int hare = nums[0];
+    
+//         public int findDuplicate(int[] nums) {       //Fluyd's algorithm  0(n)
+//          int tortoise = nums[0];                    
+//         int hare = nums[0];
         
-         do{
-            tortoise = nums[tortoise];
-            hare = nums[nums[hare]];
-        }while(tortoise!=hare);
+//          do{
+//             tortoise = nums[tortoise];
+//             hare = nums[nums[hare]];
+//         }while(tortoise!=hare);
         
-        tortoise = nums[0];
+//         tortoise = nums[0];
         
-        while(tortoise!=hare){
-            tortoise = nums[tortoise];
-            hare = nums[hare];
+//         while(tortoise!=hare){
+//             tortoise = nums[tortoise];
+//             hare = nums[hare];
+//         }
+        
+//         return hare;
+//     }
+
+class Solution {                                //array ad hash nap- 0(n) 0(1) approach
+    public int findDuplicate(int[] nums) {
+        while (nums[0] != nums[nums[0]]) {
+            int nxt = nums[nums[0]];
+            nums[nums[0]] = nums[0];
+            nums[0] = nxt;
         }
-        
-        return hare;
+        return nums[0];
     }
 }
+
+
