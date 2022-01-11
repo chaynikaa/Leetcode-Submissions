66e301d1956fd5c5608e7e4830620bbde0f5e2e0
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-01-11 23:30:28 +0530

    Time: 11 ms (21.62%), Space: 101 MB (22.03%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,25 +1,25 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
         int flag = 0;
-        for(int num:nums){
+        for(int num:nums){        //if 1 is not present that means 1 is the smallest missing +ve no.
             if(num==1)
                 flag=1;
         }
         
         if(flag==0)
             return 1;
-
+//--------------------------------------(checking when 1 is the ans case)------------------------------//
         
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<nums.length;i++){      //marking all elements exceeding the length and-ve ones as 1
             if((nums[i]>(nums.length)) || nums[i]<=0 )
                 nums[i] = 1;
         }
         
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<nums.length;i++){      
             int n = Math.abs(nums[i]);
             
             if(n==nums.length)
-                nums[0] = -Math.abs(nums[0]);
+                nums[0] = -Math.abs(nums[0]);  //doing math .abs for the case of duplicates
             else
                 nums[n] = -Math.abs(nums[n]);
         }
@@ -29,7 +29,8 @@ class Solution {
             if(nums[i]>0)
                 return i;
         }
-                        
+                    
+        
         if(nums[0]>0)
             return nums.length;
         
