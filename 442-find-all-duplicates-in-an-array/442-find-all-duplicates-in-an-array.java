111653efb5720a945500b82d541dccf2194495e0
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-01-12 01:18:21 +0530

    Time: 12 ms (34.30%), Space: 63.2 MB (22.72%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.java
@@ -3,19 +3,35 @@ class Solution {
         List<Integer> ans = new ArrayList<>();
 
         for (int num : nums) {
-            nums[Math.abs(num) - 1] *= -1;
-        }
-
-        for (int num : nums) {
-            if (nums[Math.abs(num) - 1] > 0) {
+            if (nums[Math.abs(num) - 1] < 0) { // seen before
                 ans.add(Math.abs(num));
-                nums[Math.abs(num) - 1] *= -1;
             }
+            nums[Math.abs(num) - 1] *= -1;
         }
 
         return ans;
     }
 }
+
+
+// class Solution {
+//     public List<Integer> findDuplicates(int[] nums) {
+//         List<Integer> ans = new ArrayList<>();
+
+//         for (int num : nums) {
+//             nums[Math.abs(num) - 1] *= -1;
+//         }
+
+//         for (int num : nums) {
+//             if (nums[Math.abs(num) - 1] > 0) {
+//                 ans.add(Math.abs(num));
+//                 nums[Math.abs(num) - 1] *= -1;  //prevent duplicates and making the indexes valid simulataneously
+//             } 
+//         }
+
+//         return ans;
+//     }
+// }
 // class Solution {
 //     public List<Integer> findDuplicates(int[] nums) {
         
