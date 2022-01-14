8eb21ec9f919d095698c7c90f2df3b35e277b065
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-01-14 15:50:42 +0530

    Time: 1 ms (99.94%), Space: 54.3 MB (14.47%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -4,8 +4,8 @@
         int major=num[0], count = 0;
         for(int i=0; i<num.length;i++){
             if(count==0){
-                count++;
                 major=num[i];
+                count++;
             }else if(major==num[i]){
                 count++;
             }
