cb26f3051c03a83959a0045a0eba8175338adc52
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-01-17 17:05:54 +0530

    Time: 54 ms (29.37%), Space: 84.5 MB (17.98%) - LeetHub

--- 895-maximum-frequency-stack/895-maximum-frequency-stack.java
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.java
@@ -12,24 +12,24 @@ class FreqStack {
     
     public void push(int val) {
         
-        int f = freq.getOrDefault(val,0);
-        f++;
-        freq.put(val,f);
+        int f = freq.getOrDefault(val,0);   //taking the value from frequency map
+        f++;                                //updating it
+        freq.put(val,f);                    //putting it back in the map
         
-        if(group.containsKey(f)==false)
+        if(group.containsKey(f)==false)     //if it doesnt exist-> make new empty stack 
             group.put(f,new Stack<Integer>());
         
-        group.get(f).push(val);
-        max = Math.max(max,f);
+        group.get(f).push(val);           //push the value in the stack
+        max = Math.max(max,f);            //update the max
     }
     
     public int pop() {
-        int ans = group.get(max).pop();
+        int ans = group.get(max).pop();   //pop out the max freq element from the stack
         
-        freq.put(ans,freq.get(ans)-1);
+        freq.put(ans,freq.get(ans)-1);   //update its no. in the frequency stack
         
-        if(group.get(max).size()==0)
-            max--;
+        if(group.get(max).size()==0)     //if the stack becomes empty after that
+            max--;                      //decrease the max by 1
         
         return ans;
         
