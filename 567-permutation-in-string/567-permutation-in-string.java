4310e43194b3eb1961e799bf50a55dc86e49aa5e
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-01-23 15:59:35 +0530

    Time: 8 ms (68.34%), Space: 40.1 MB (33.15%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        
+        if(s1.length()>s2.length())
+            return false;
+        
+        int[] map1 = new int[26];
+        int[] map2 = new int[26];
+        
+        for(int i=0;i<s1.length();i++){
+            map1[s1.charAt(i)-'a']++;
+            map2[s2.charAt(i)-'a']++;  
+        }
+        
+        for(int i=0;i<s2.length()-s1.length();i++){
+            if(matches(map1,map2))
+                return true;
+            map2[s2.charAt(i+s1.length())-'a']++;
+            map2[s2.charAt(i)-'a']--;
+        }
+       return matches(map1,map2); 
+    }   
+    
+    public boolean matches(int[]map1,int[]map2){
+        for(int i=0;i<26;i++){
+            if(map1[i]!=map2[i])
+                return false;
+        }
+        return true;
+    }
+}
