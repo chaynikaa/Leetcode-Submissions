f9915f3c396c104e1555ae03c6d3097af8f1f56a
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-01-23 16:01:03 +0530

    Time: 13 ms (52.37%), Space: 40.2 MB (33.15%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -1,28 +1,28 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         
-        if(s1.length()>s2.length())
+        if(s1.length()>s2.length())   //if length is greater it cant be a substring
             return false;
         
         int[] map1 = new int[26];
         int[] map2 = new int[26];
         
-        for(int i=0;i<s1.length();i++){
+        for(int i=0;i<s1.length();i++){   //freq array of both the strings
             map1[s1.charAt(i)-'a']++;
             map2[s2.charAt(i)-'a']++;  
         }
         
         for(int i=0;i<s2.length()-s1.length();i++){
-            if(matches(map1,map2))
+            if(matches(map1,map2))                  //if is anagram. return true.
                 return true;
-            map2[s2.charAt(i+s1.length())-'a']++;
-            map2[s2.charAt(i)-'a']--;
+            map2[s2.charAt(i+s1.length())-'a']++;   //else update the next char
+            map2[s2.charAt(i)-'a']--;               //and remove the starting char
         }
-       return matches(map1,map2); 
+       return matches(map1,map2);                   //check for last pair
     }   
     
-    public boolean matches(int[]map1,int[]map2){
-        for(int i=0;i<26;i++){
+    public boolean matches(int[]map1,int[]map2){   //is anagram function
+        for(int i=0;i<26;i++){    
             if(map1[i]!=map2[i])
                 return false;
         }
