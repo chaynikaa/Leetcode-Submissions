f0e03e744ca78e9a7298c18f48d20c7fafd40e4a
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-01-25 16:34:03 +0530

    Time: 2 ms (26.97%), Space: 42.1 MB (42.27%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -15,20 +15,39 @@
  */
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        inorder(root,arr);
+        Stack<TreeNode> st = new Stack<>();
         
-        return arr.get(k-1);
-    }
-    
-    private ArrayList<Integer> inorder(TreeNode root, ArrayList<Integer> ans){
-        if(root == null)
-            return ans;
-        
-        inorder(root.left,ans);
-        ans.add(root.val);
-        inorder(root.right,ans);
-        
-        return ans;
+        while(true){
+            while(root!=null){
+                st.push(root);
+                root = root.left;
+            }
+            root = st.pop();
+            if(--k==0)
+                return root.val;
+            else
+                root = root.right;
+        }
     }
 }
+
+
+// class Solution {
+//     public int kthSmallest(TreeNode root, int k) {
+//         ArrayList<Integer> arr = new ArrayList<>();
+//         inorder(root,arr);
+        
+//         return arr.get(k-1);
+//     }
+    
+//     private ArrayList<Integer> inorder(TreeNode root, ArrayList<Integer> ans){
+//         if(root == null)
+//             return ans;
+        
+//         inorder(root.left,ans);
+//         ans.add(root.val);
+//         inorder(root.right,ans);
+        
+//         return ans;
+//     }
+// }
