158efbec455bc78488badb43009ebdd57f365835
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-01-26 21:10:55 +0530

    Time: 11 ms (80.51%), Space: 41.4 MB (85.56%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,8 +1,8 @@
 class Solution {
     
-    private static int[][] dir = new int[][]{ {1,0},{0,1},{-1,0},{0,-1}};
+    private static int[][] dir = new int[][]{ {1,0},{0,1},{-1,0},{0,-1}};  //direction array
     
-        private static class Pair {
+    private static class Pair {     //pair class
         int x;
         int y;
 
@@ -14,28 +14,28 @@ class Solution {
     
     public int[][] updateMatrix(int[][] mat) {
         
-        Queue<Pair> q = new LinkedList<>();
+        Queue<Pair> q = new LinkedList<>();  //new queue
         
-        for(int i=0;i<mat.length;i++){
+        for(int i=0;i<mat.length;i++){   
             for(int j=0; j<mat[0].length;j++){
-                if(mat[i][j]==0)
+                if(mat[i][j]==0)             //additing the indices of 0
                     q.add(new Pair(i,j));
                 else
-                    mat[i][j]=-1;
+                    mat[i][j]=-1;           //setting the others to -1
             }
         }
         
         while(q.size()>0){
             
-            Pair rem = q.remove();
+            Pair rem = q.remove();    //removing the 0th element
             
             for(int i=0;i<dir.length;i++){
-                int temprow = rem.x + dir[i][0];
+                int temprow = rem.x + dir[i][0];    //traversing in all possible valid directions
                 int tempcol = rem.y+dir[i][1];
                 
                 if(temprow>=0 && tempcol>=0 && temprow<mat.length && tempcol<mat[0].length && mat[temprow][tempcol]<0){
-                    mat[temprow][tempcol] = mat[rem.x][rem.y] + 1;
-                    q.add(new Pair(temprow,tempcol));
+                    mat[temprow][tempcol] = mat[rem.x][rem.y] + 1;   //means it is solvable. put the distance.
+                    q.add(new Pair(temprow,tempcol));                //add the solved one for next iteration
                 }
             }
         }
