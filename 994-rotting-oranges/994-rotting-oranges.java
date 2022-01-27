5af1485e85b50f14f12ef56f46e8caeafd5d5ce8
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-01-27 20:18:53 +0530

    Time: 3 ms (70.36%), Space: 43.2 MB (5.13%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -1,47 +1,54 @@
 class Solution {
-    int n;
-    int m;
-    int fresh;
-    int minute;
-    int[][] steps = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-    private void rottenBFS(int[][]g, Queue<int[]> q){
-        int size = q.size();
-        for(int i = 0; i < size; i++){
-            int[] p = q.poll();
-            
-            for(int[] s: steps){
-                int x = s[0] + p[0];
-                int y = s[1] + p[1];
-                
-                if(x < 0 || y < 0 || x >= n || y >= m || g[x][y] == 2 || g[x][y] == 0)
-                    continue;
-                g[x][y] = 2;
-                fresh--;
-                q.offer(new int[]{x,y});
-            }
+    private static int[][]dir = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+    private static class Pair{
+        int x;
+        int y;
+        
+        Pair(int x,int y){
+            this.x = x;
+            this.y = y;
         }
-        minute++;
-        if(!q.isEmpty())
-            rottenBFS(g, q);
     }
     public int orangesRotting(int[][] grid) {
-        n = grid.length;
-        m = grid[0].length;
-        minute = 0;
-        fresh = 0;
-        Queue<int[]> queue = new LinkedList<>();
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                 if(grid[i][j] == 2){
-                     queue.offer(new int[]{i,j});
-                 }
-                if(grid[i][j] == 1)
+        Queue<Pair> q = new LinkedList<>();
+        int fresh = 0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==2)
+                    q.add(new Pair(i,j));
+                if(grid[i][j]==1)
                     fresh++;
             }
         }
         
-        rottenBFS(grid, queue);
-        return fresh >= 1? -1:minute-1;
+        if(fresh==0)
+            return 0;
         
+        int time = -1;
+        
+        while(q.size()>0){
+            int size = q.size();
+            time++;
+            
+            while(size-->0){
+                Pair rem = q.remove();
+                for(int i=0;i<dir.length;i++){
+                    int tempr = rem.x + dir[i][0];
+                    int tempc = rem.y + dir[i][1];
+                    
+                    if(tempr>=0 && tempc>=0 && tempr<grid.length && tempc<grid[0].length && grid[tempr][tempc]==1){
+                        grid[tempr][tempc]=0;
+                        q.add(new Pair(tempr,tempc));
+                        fresh--;
+                    }
+                }
+            }
+        }
+                    
+            if(fresh==0)
+                return time;
+            else
+                return -1;
+            
     }
 }
