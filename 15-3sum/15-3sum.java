1c99c549322488487fbe43a2fc78886f1f082348
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-02-02 13:41:28 +0530

    Time: 585 ms (15.93%), Space: 121.1 MB (10.71%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,27 +1,68 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-         
-        Set<List<Integer>> res = new HashSet<>();  
-        
-        Set<Integer> dup = new HashSet<>();
-
-        HashMap<Integer,Integer> map = new HashMap<>();   
-        
-        for(int i=0;i<nums.length;i++){
-            if(dup.add(nums[i])==true){  //REMOVING DUPLICATES
-                for(int j=i+1;j<nums.length;j++){
-                    
-                    int target = -(nums[i]+nums[j]);
-                    
-                    if(map.containsKey(target) && map.get(target)==i){
-                        List<Integer> ans = Arrays.asList(nums[i],nums[j],target);
-                        Collections.sort(ans);
-                        res.add(ans);
+        Set<List<Integer>> res = new HashSet<>();
+        Set<Integer> dups = new HashSet<>();
+        Map<Integer, Integer> seen = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i)
+            if (dups.add(nums[i])) {
+                for (int j = i + 1; j < nums.length; ++j) {
+                    int complement = -nums[i] - nums[j];
+                    if (seen.containsKey(complement) && seen.get(complement) == i) {
+                        List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
+                        Collections.sort(triplet);
+                        res.add(triplet);
                     }
-                    map.put(nums[j],i);
+                    seen.put(nums[j], i);
                 }
             }
-        }
-      return new ArrayList(res);  
+        return new ArrayList(res);
     }
 }
+
+// class Solution {            //TLE 
+//     public List<List<Integer>> threeSum(int[] nums) {
+         
+//         Set<List<Integer>> res = new HashSet<>();  
+        
+//         HashMap<Integer,Integer> map = new HashMap<>(); 
+        
+//         for(int i=0;i<nums.length;i++){
+//             int put = map.getOrDefault(nums[i],0);
+//             put++;
+//             map.put(nums[i],put);   
+//         }
+        
+//         for(int i=0;i<nums.length;i++){
+//             int count = map.get(nums[i]);
+//             count--;
+//             if(count==0)
+//                 map.remove(nums[i]);
+//             else
+//                 map.put(nums[i],count);
+//                     //REMOVING DUPLICATES
+//                 for(int j=i+1;j<nums.length;j++){
+//                   int co = map.get(nums[j]);
+//                     co--;
+//                      if(co==0)
+//                             map.remove(nums[j]);
+//                     else
+//                             map.put(nums[j],co);
+                                
+                    
+//                     int target = -(nums[i]+nums[j]);
+                    
+//                     if(map.containsKey(target)){
+//                         List<Integer> ans = Arrays.asList(nums[i],nums[j],target);
+//                         Collections.sort(ans);
+//                         res.add(ans);
+//                     }
+//                  int s = map.getOrDefault(nums[j], 0);
+//                 map.put(nums[j], s + 1);
+//                 }
+//             int p = map.getOrDefault(nums[i], 0);
+//              map.put(nums[i], p + 1);
+//         }
+        
+//       return new ArrayList(res);  
+//     }
+// }
