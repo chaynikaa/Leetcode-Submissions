127a5f27b1e39c71f2d2c73235ba900082f6d93e
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-02-14 14:20:34 +0530

    Time: 152 ms (48.04%), Space: 117.1 MB (7.25%) - LeetHub

--- 127-word-ladder/127-word-ladder.java
+++ 127-word-ladder/127-word-ladder.java
@@ -1,41 +1,43 @@
 class Solution {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> word=new HashSet(wordList);//it will be easy to check if wordlist contains that word
-        Queue<String> q=new LinkedList<>();
-        Set<String> visited=new HashSet<>();//to keep track of visited words;
-        q.add(beginWord);
-        visited.add(beginWord);
-        
-        int level=0;
-        while(!q.isEmpty())
-        {
-            for(int i=q.size();i>0;i--)
-            {
-                String curr_word=q.poll();
-                if(curr_word.equals(endWord))
-                    return level+1;
-                
-                char[] c=curr_word.toCharArray();
-                for(int j=0;j<c.length;j++)//to replace every character of that word and check if it is present in wordlist
-                {
-                   char temp=c[j];
-                   for(char ch='a'; ch<='z'; ch++)
-                   {
-                       c[j]=ch;
-                       String word_formed=String.valueOf(c);//if this newly formed word isnt visited and also present in the wordlist add it to the queue
-                       if(!visited.contains(word_formed) && word.contains(word_formed))
-                       {
-                           q.add(word_formed);
-                           visited.add(word_formed);
-                       }
-                   }
-                    c[j]=temp;
-                }
-            }
-            
-            level++;//moving to the next level
+   public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+		//set to hold all words
+        Set<String> dict = new HashSet<>(wordList);
+        //if endword does not exists return 0
+		if(!dict.contains(endWord)) {
+            return 0;
         }
         
+        int level=1;
+        Queue<String> queue = new LinkedList<>();
+        queue.add(beginWord);
+		//iterate bfs level wise
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            while(size-->0) {
+                String word = queue.poll();
+				//if current word is end word return count
+                if(word.equals(endWord)) {
+                    return level;
+                }
+				//iterate and try every character change for ith index in word
+                for(int i=0;i<word.length();i++) {
+                    for(int ch='a';ch<='z';ch++) {
+                        char[] arr = word.toCharArray();
+						//don't need to replace same character again.
+                        if((char)ch!=arr[i]) {
+                            arr[i]=(char)ch;
+                            String newWord = new String(arr);
+								//if set contains newWord add it to queue and remove it from set to avoid further processing
+                            if(dict.contains(newWord)) {
+                                queue.add(newWord);
+                                dict.remove(newWord);
+                            }
+                        }
+                    }        
+                }
+            }
+            level++;
+        }
         return 0;
     }
 }
