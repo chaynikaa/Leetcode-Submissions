0b11a35494e2b5941081478f4a019b2d463a9fd9
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-02-27 18:19:44 +0530

    Time: 2 ms (78.02%), Space: 43.7 MB (23.62%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
@@ -13,50 +13,50 @@
  *     }
  * }
  */
-class Pair{
+class Pair{                  //pair class
     TreeNode node;
     int num;
-    Pair(TreeNode _node ,int _num){
-        num = _num;
-        node = _node;
+    Pair(TreeNode node ,int num){
+        this.num = num;
+        this.node = node;
     }
 }
 
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
         
-        if(root == null)
+        if(root == null)       
             return 0;
         
         int ans = 0;
-        
+        //level order traversal
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(root,0));
+        q.add(new Pair(root,0));         
         
         while(q.size()>0){
             int size = q.size();
-            int min = q.peek().num;
+            int min = q.peek().num;   
             int first = 0,last=0;
             
             for(int i=0;i<size;i++){
                 int curr = q.peek().num-min;
                 TreeNode node = q.peek().node;
-                q.remove();
+                q.remove();    //removing the exisiting node
                 
-                if(i==0)
+                if(i==0)        //first index of a particular level
                     first = curr;
                 
-                if(i==size-1)
+                if(i==size-1)   //last index of a particular level
                     last = curr;
                 
                 if(node.left!=null)
-                    q.add(new Pair(node.left,curr*2 + 1));
+                    q.add(new Pair(node.left,curr*2 + 1));   //setting the value of the nodes
                 
                 if(node.right!=null)
                     q.add(new Pair(node.right, curr*2 + 2));
             }
             
-            ans = Math.max(ans,last-first+1);
+            ans = Math.max(ans,last-first+1);   //calculating the max
         }
       return ans;  
     }
