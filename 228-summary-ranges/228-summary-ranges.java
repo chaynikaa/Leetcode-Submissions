ab5e21bf3b87c987ad79f7c0b2f5d08c4102ffd1
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-02-28 18:47:16 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (21.59%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.java
+++ 228-summary-ranges/228-summary-ranges.java
@@ -2,24 +2,24 @@ class Solution {
     public List<String> summaryRanges(int[] nums) {
         
         List<String> ans = new ArrayList<>();
-        if(nums.length==0)
+        if(nums.length==0)    //no range at all
             return ans;
         
         StringBuilder sb = new StringBuilder();
      
         int i=0;
         while(i<nums.length){
-            int first = nums[i];
-            sb.append(first);
+            int first = nums[i]; 
+            sb.append(first);   //adding the first to the string builder
             i++;
-            while(i<nums.length && nums[i]==1+nums[i-1]){
+            while(i<nums.length && nums[i]==1+nums[i-1]){   //if the sequence exists
                 i++;
             }
-            if(first!=nums[i-1])
+            if(first!=nums[i-1])    //checking if the first and last are same or not
                 sb.append("->").append(nums[i-1]);
             
-            ans.add(sb.toString());
-            sb.setLength(0);
+            ans.add(sb.toString()); //adding to the final ans
+            sb.setLength(0);   //setting the string builder to zero again
         }
       return ans;  
     }
