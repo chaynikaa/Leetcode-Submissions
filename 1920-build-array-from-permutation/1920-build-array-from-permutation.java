8cafd06bcd6f33dcc8f856ecc563cce54fcca7a2
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-02-28 21:18:15 +0530

    Time: 3 ms (9.94%), Space: 52.9 MB (37.21%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.java
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -1,12 +1,26 @@
+// class Solution {
+//     public int[] buildArray(int[] nums) {
+        
+//       int[] ans = new int[nums.length]; 
+//       for(int j=0,i=0;i<nums.length;i++,j++){
+//          int x = nums[i];
+//          ans[j]=nums[x];
+//      }
+//         return ans;
+        
+//     }
+// }
+
 class Solution {
-    public int[] buildArray(int[] nums) {
-        
-      int[] ans = new int[nums.length]; 
-      for(int j=0,i=0;i<nums.length;i++,j++){
-         int x = nums[i];
-         ans[j]=nums[x];
-     }
-        return ans;
-        
-    }
+  public int[] buildArray(int[] nums) {
+    int n = nums.length;
+
+    for (int i = 0; i < n; i++)
+      nums[i] = nums[i] + n * (nums[nums[i]] % n);
+
+    for (int i = 0; i < n; ++i)
+      nums[i] /= n;
+
+    return nums;
+  }
 }
