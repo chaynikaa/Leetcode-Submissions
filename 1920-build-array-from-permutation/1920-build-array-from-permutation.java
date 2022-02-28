c4581156d07bb78fb362e4be34cd967375f47c7e
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-02-28 21:23:24 +0530

    Time: 2 ms (45.96%), Space: 54.1 MB (6.66%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.java
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -18,7 +18,7 @@ class Solution {
     for (int i = 0; i < n; i++)
       nums[i] = nums[i] + n * (nums[nums[i]] % n);
 
-    for (int i = 0; i < n; ++i)
+    for (int i = 0; i < n; i++)
       nums[i] /= n;
 
     return nums;
