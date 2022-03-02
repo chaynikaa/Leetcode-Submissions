c7c11ccf80d8488fbda34cd376bcc9a6d8a90e7e
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-03-02 16:56:01 +0530

    Time: 1 ms (88.36%), Space: 42.2 MB (18.72%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        
+        if(s.length()==0)
+            return true;
+        
+        if(s.length()>t.length())
+            return false;
+        
+        int j=0;
+        
+        for(int i=0;i<t.length();i++){
+            char ch = s.charAt(j);
+            
+            if(t.charAt(i)==ch){
+                j++;
+            }
+           if(j==s.length())
+            return true;
+            
+        }
+        
+            return false;
+    }
+}
