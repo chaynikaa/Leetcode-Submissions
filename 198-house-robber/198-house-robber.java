aa08481982abd2cfe0836714b45e5d3bd72b37a4
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-03-05 12:59:07 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (22.75%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -2,11 +2,17 @@
     public int rob(int[] nums) {
         
         int n = nums.length;
-        int dp[] = new int[n];
-        dp[0]=nums[0];
+        
+        if(n==0)
+            return 0;
+        
         if(n==1){
             return nums[0];
         }
+        
+        int dp[] = new int[n];
+        dp[0]=nums[0];
+
         
         dp[1] = Math.max(nums[0],nums[1]);
         
@@ -18,3 +24,4 @@
 
     }
 }
+
