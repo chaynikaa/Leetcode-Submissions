25f10a6f6eeec91d3fcc9a32915ec1531ac7e86e
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-08-29 23:48:06 +0530

    Time: 6 ms (39.74%), Space: 57.9 MB (27.62%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,29 +1,33 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        boolean visited[][]=new boolean [grid.length][grid[0].length];
-        int count=0;
+        boolean visited[][] = new boolean[grid.length][grid[0].length];
+        int count = 0;
+        
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]=='1' && visited[i][j]!=true){
-                    dfs(grid,i,j,visited);
+                    dfs(grid,visited,i,j);
                     count++;
                 }
+                    
             }
         }
+        
         return count;
         
     }
     
-    private void dfs(char[][]grid, int sr, int sc, boolean visited[][]){
-        if(sr<0 || sc<0 || sr>=grid.length || sc>=grid[0].length || grid[sr][sc]!='1' || visited[sr][sc]==true)
+    public void dfs(char[][]grid,boolean visited[][],int sr, int sc){
+        
+        if(sr<0 || sc<0 || sr>=grid.length || sc>=grid[0].length || visited[sr][sc]!=false || grid[sr][sc]=='0')
             return;
         
-        visited[sr][sc]=true;
+        visited[sr][sc] = true;
         
-        dfs(grid,sr+1,sc,visited);
-        dfs(grid,sr-1,sc,visited);
-        dfs(grid,sr,sc+1,visited);
-        dfs(grid,sr,sc-1,visited);
+        dfs(grid,visited,sr+1,sc);
+        dfs(grid,visited,sr,sc+1);
+        dfs(grid,visited,sr-1,sc);
+        dfs(grid,visited,sr,sc-1);
 
     }
 }
