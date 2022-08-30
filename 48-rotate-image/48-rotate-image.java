30fd4b0b542301542a47a3f3a5a8d061523bbe24
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-08-30 12:41:07 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (21.32%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,28 +1,28 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        for(int i = 0 ;i <n;i++){           //SWAPPING ACROSS DIAG ELEMENTS
-            for(int j = i; j<n ; j++){
+        
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i;j<matrix[0].length;j++){
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
-                matrix[j][i]=temp;
-           }
+                matrix[j][i] = temp;
+            }
         }
         
-        for(int i = 0 ;i<n;i++){       //REVERSING THE COLUMNS OF THE MATRIX
-                int li=0;
-                int ri=n-1;
-                
-                while(li<ri){
-                    
-                    int temp = matrix[i][li];
-                    matrix[i][li] = matrix[i][ri];
-                    matrix[i][ri] = temp;
-                    li++;
-                    ri--;
-                }
+        
+        for(int i=0;i<matrix.length;i++){
+            int li = 0;
+            int ri = matrix[0].length-1;
             
+            while(li<ri){
+                int temp =  matrix[i][li];
+                matrix[i][li] = matrix [i][ri];
+                matrix[i][ri] = temp;
+                li++;
+                ri--;
+            }
         }
+
         
         
     }
