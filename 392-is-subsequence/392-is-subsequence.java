d1b42b53b665dbcef52f128e9a92146d6003bf8c
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2022-08-30 13:01:38 +0530

    Time: 3 ms (18.28%), Space: 42.3 MB (24.01%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -1,54 +1,23 @@
-// class Solution {
-//     public boolean isSubsequence(String s, String t) {
-        
-//         if(s.length()==0)
-//             return true;
-        
-//         if(s.length()>t.length())
-//             return false;
-        
-//         int j=0;
-        
-//         for(int i=0;i<t.length();i++){
-//             char ch = s.charAt(j);
-            
-//             if(t.charAt(i)==ch){
-//                 j++;
-//             }
-            
-//             if(j==s.length())
-//             return true;
-            
-//         }
-//             return false;
-//     }
-// }
-
-
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        
         if(s.length()==0)
             return true;
         
-    
-        return check(s,t,0,0);
-    }
-    
-    public boolean check(String s, String t, int m, int n){
-        if(m==s.length())
-            return true;
-        
-        if(m<s.length() && n==t.length()) 
+        if(s.length()>t.length())
             return false;
         
-        if(s.charAt(m)==t.charAt(n)){
-            return check(s,t,m+1,n+1);
+        int j = 0;
+        
+        for(int i=0;i<t.length();i++){
+            char ch = t.charAt(i);
+            
+            if(ch == s.charAt(j))
+                j++;
+            
+            if(j==s.length())
+                return true;
         }
         
-        else{
-            return check(s,t,m,n+1);
-        }
+        return false;
     }
 }
-
