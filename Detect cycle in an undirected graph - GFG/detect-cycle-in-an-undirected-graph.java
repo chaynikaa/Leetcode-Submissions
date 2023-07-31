e91d094445456d5564cc4babb4bc5c7750af5b09
Chaynika Arora <84374076+chaynikaa@users.noreply.github.com>
2023-07-31 17:48:44 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -36,28 +36,40 @@ class Solution {
     // Function to detect cycle in an undirected graph.
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
         // Code here
-        int[] visited = new int[V+1];
+        int[] visited = new int[V];
         for(int i=0;i<V;i++){
-            if(visited[i]==0){
-                if(dfs(i,-1,adj,visited))
-                return true;
-            }
+            if(visited[i]==0)
+            if(check(V,adj,visited,i))
+            return true;
         }
         return false;
     }
     
-    public boolean dfs(int node, int parent, ArrayList<ArrayList<Integer>> adj, int[] visited){
-        visited[node] = 1;
+    public boolean check(int V, ArrayList<ArrayList<Integer>>adj, int[]visited , int src){
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(src,-1));
+        visited[src] = 1;
         
-        for(int neighbour:adj.get(node)){
-            if(visited[neighbour]==0){
-             if(dfs(neighbour,node,adj,visited))
-             return true;
-            } else {
-                if(neighbour!=parent)
-                return true;
+        while(q.size()>0){
+            Pair node = q.poll();
+            for(int i:adj.get(node.node)){
+                if(visited[i]==0){
+                    visited[i]=1;
+                    q.add(new Pair(i,node.node));
+                } else {
+                    if(i!=node.parent)
+                    return true;
+                }
             }
         }
         return false;
+    }
+}
+class Pair{
+    int node;
+    int parent;
+    Pair(int node, int parent){
+        this.node = node;
+        this.parent = parent;
     }
 }
